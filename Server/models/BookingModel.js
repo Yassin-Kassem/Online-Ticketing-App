172dcaa8635cd7633fd7bd26815a638b02c69404
yousef <youssef_nabil2006@outlook.com>
2025-03-04 22:34:31 +0200

    intial commit

---
+++ Server/models/BookingModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const bookingSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User', // Reference to the user who made the booking
+      required: true,
+    },
+    event: {
+      type: Schema.Types.ObjectId,
+      ref: 'Event', // Reference to the event being booked
+      required: true,
+    },
+    numberOfTickets: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+  });
