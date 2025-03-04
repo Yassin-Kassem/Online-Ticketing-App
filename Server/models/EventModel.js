9c65db49fc3ea3f481d00db30195a73ceea724ab
MadanyCode <ibrahimmadany5@gmail.com>
2025-03-04 22:45:17 +0200

    initial commit (event)

---
+++ Server/models/EventModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  date: { type: Date, required: true },
+  location: { type: String, required: true },
+  category: { type: String, required: true },
+  image: { type: String }
+});
