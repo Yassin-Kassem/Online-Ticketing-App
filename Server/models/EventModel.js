f621b72fa29e29d5d7ccb9ba8b9227fafcb8770d
Yassin-Kassem <yassinkassem29@gmail.com>
2025-03-06 23:47:38 +0200

    fix

--- Server/models/EventModel.js
+++ Server/models/EventModel.js
@@ -6,5 +6,12 @@ const eventSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   location: { type: String, required: true },
   category: { type: String, required: true },
-  image: { type: String }
+  image: { type: String },
+  ticketPricing: { type: Number, required: true },
+  totalTicketsAvailable: { type: Number, required: true },
+  remainingTickets: { type: Number, required: true },
+  organizer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now },
 });
+
+module.exports = mongoose.model('Event', eventSchema);
