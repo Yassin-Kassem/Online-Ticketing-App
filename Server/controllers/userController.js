db667eb049e11a35846b74cd52433823782a19e1
Loaywaleed2005 <Loaywaleed2005@gmail.com>
2025-04-17 14:46:13 +0200

    rest of user methods and middleware implementation

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -120,10 +120,58 @@ const forgotPassword = async (req, res, next) => {
         next(error);
     }
 };
+
+const getUserById = async (req, res, next) => { //admin access only
+    try {
+        const user = await User.findById(req.params.id);
+        if(!user){
+            res.status(400);
+            throw new Error("User not Found");
+        }
+        return res.status(200).json(message = user);
+    } catch (error) {
+        next(error)
+    }
+};
+
+const updateUserRole = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            { role: req.body.role },
+            { new: true, runValidators: true }
+        );
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' }); // Use 404 for "not found"
+        }
+
+        return res.status(200).json({ message: 'New Role: ' + user.role }); // Properly format the response
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteUser = async(req, res, next) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            res.status(400);
+            throw new Error("User not found");
+        }
+        return res.status(200).json({message: "User deleted successfully"});
+    } catch (error) {
+        next(error);
+    }
+}
+
   module.exports = {
     getAllUsers,
     registerUser,
     loginUser,
     LogoutUser,
     forgotPassword,
+    getUserById,
+    updateUserRole,
+    deleteUser,
   }
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { getAllUsers, registerUser, loginUser, LogoutUser, forgotPassword} = require('../controllers/userController');
+const { getAllUsers, registerUser, loginUser, LogoutUser, forgotPassword, getUserById, updateUserRole, deleteUser} = require('../controllers/userController');
 const { auth } = require('../middleware/auth');
+const { validateRole, authoriseOnly } = require('../middleware/authorisationMiddleware');
 const router = express.Router();
 
 router.get("/", auth, getAllUsers);
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/logout', LogoutUser);
-router.post("/forgetPassword", forgotPassword)
+router.post("/forgetPassword", forgotPassword);
+router.get("/:id", getUserById);
+router.put("/:id", validateRole, updateUserRole);
+router.delete("/:id", auth, authoriseOnly(['System Admin']), deleteUser);
 
 module.exports = router;
