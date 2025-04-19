c40b7c6bf091b28032b2e6a8ebbbec23b6c466a4
MadanyCode <ibrahimmadany5@gmail.com>
2025-04-19 15:06:38 +0200

    b

--- Server/controllers/eventController.js
+++ Server/controllers/eventController.js
@@ -68,31 +68,7 @@ const getDetailsOfEvent = async(req, res, next) =>{
         next(error);
     }
 }
-const updateEvent = async (req, res, next) => {
-    try {
-    
-      const updateData = {
-        date: req.body.date,
-        location: req.body.location,
-        totalTicketsAvailable: req.body.totalTicketsAvailable,
-      };
-  
-   
-      const event = await Event.findByIdAndUpdate(
-        req.params.id,
-        updateData,
-        { new: true, runValidators: true }
-      );
-  
-      if (!event) {
-        return res.status(404).json({ message: 'Event not found' });
-      }
-  
-      return res.status(200).json(event);
-    } catch (error) {
-      next(error);
-    }
-  };
+
 const deleteEvent = async(req, res, next) =>{
         try {
             const event = await Event.findByIdAndDelete(req.params.id);
